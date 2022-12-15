c8082870d80731285157979f485b1df7de43411c
CemGurhan <cemg@live.co.uk>
2022-12-15 07:33:20 +0000

    cleaned up validation_wrapper.py

--- backend/tx_validator/src/validation_wrapper.py
+++ backend/tx_validator/src/validation_wrapper.py
@@ -1,18 +1,7 @@
 import sys
 import numpy as np
-import pandas as pd 
-import os
-import warnings
-# from io import BytesIO
-from numproto import ndarray_to_proto, proto_to_ndarray
-import tensorflow as tf
 import service_pb2 as tx
-import time as timer
 import requests
-
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"      # To disable using GPU
-#tf.get_logger().setLevel('ERROR')
-#warnings.filterwarnings('ignore')
 
 MODELS_DIR = '../tx_validator/src/models/'
 
@@ -25,32 +14,13 @@ import importlib
 model_mod = importlib.import_module('models.%s.validate'%model_id)
 
 def parse_gradients(gradients_path, isRoundOne):
-    if (isRoundOne == "1") : # just make this a variable instad of sys.argv in the argument for parse_gradients
+    if (isRoundOne == "1") : 
         gradient = open(gradients_path, "rb").read()
         transaction = tx.TxShareUpdates()
         transaction.ParseFromString(gradient)
      
         return transaction.gradients
     elif (isRoundOne == "0") :
-        # gradient = open(gradients_path, "r", encoding="utf-8").read()
-        # print("BUFFER SIZE: ", len(gradient))
-        # array = np.frombuffer(gradient, dtype=np.dtype(np.float32))
-
-        # array = np.fromfile(gradients_path, dtype="float32")
-
-        # print("ARRAY: ", array)
-        
-        # return array
-
-        # gradients = open(gradients_path).read()
-        # # print("GRADIENTS: ", gradients)
-        # g = gradient.decode("utf-8")
-        # gradientsPenultimate = gradient.replace("[","")
-        # gradient = gradientsPenultimate.replace("]","")
-        # split = gradient.split(",")
-        # split = [float(element) for element in split]
-        # return np.array(split)
-        
         formattedURL = "http://127.0.0.1:9000/api/services/ml_service/v1/models/latestmodel_raw?version={}".format(sys.argv[8])
         response = requests.get(formattedURL)
 
@@ -62,8 +32,6 @@ def parse_gradients(gradients_path, isRoundOne):
         split = [float(element) for element in latest_model_list]
         return np.array(split)
 
-
-
 def send_valid(is_valid):
     verdict = 'valid' if is_valid else 'invalid'
     print("VERDICT=" + verdict + "ENDVERDICT")
@@ -71,7 +39,6 @@ def send_valid(is_valid):
 def send_score(score):
     print("SCORE" + str(score) + "ENDSCORE")
 
-#data_validation = pd.read_csv(data_dir)
 gradients = parse_gradients(sys.argv[4], "1")
 newModel_flag = str(sys.argv[1])
 if (newModel_flag == "true"):
@@ -86,14 +53,8 @@ if newModel_flag:
 else:
     base_model = parse_gradients(sys.argv[3], "0")
     evaluate_model = base_model + gradients
-    # evaluate_model = np.array(evaluate_model_list)
-# if newModel_flag:
-#     np.random.seed(0)
-#     base_model = np.random.uniform(low = -0.09, high = 0.09, size = len(gradients)).tolist()
-# else: 
-#     base_model = parse_gradients(sys.argv[3])
+   
 min_score = float(sys.argv[5])
-# evaluate_model = base_model + gradients
 score = model_mod.compute_validation_score(evaluate_model, data_dir)
 is_valid = score >= min_score
 
