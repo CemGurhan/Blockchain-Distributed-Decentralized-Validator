86983f34a0fbe9f37edb32cc2ca0559fdab0a71a
CemGurhan <cemg@live.co.uk>
2022-12-19 13:51:34 +0000

    added comments to aid understanding run_local_test script

--- backend/test_scripts/run_local_test.sh
+++ backend/test_scripts/run_local_test.sh
@@ -9,7 +9,7 @@ validator_reciever_port=$(($validator_peer_port+$number_of_validators))
 peer_reciever_flag=""
 current_reciever_port=$(($validator_peer_port+$number_of_validators))
 
-# create reciever address flag and run configure_node.sh for first validator 
+# create reciever address flag, and run configure_node.sh, for first validator 
 for i in $(seq 0 $(($number_of_validators - 2))); do
         peer_reciever_flag+="-a 0.0.0.0:"
         validator_reciever_port=$(($validator_reciever_port+$((i+1))))
@@ -18,6 +18,7 @@ for i in $(seq 0 $(($number_of_validators - 2))); do
         validator_reciever_port=$(($validator_reciever_port-$((i+1))))
 done
 validator_reciever_port=$(($validator_peer_port+$number_of_validators))
+# change current reciever port to next validators reciever port 
 current_reciever_port=$(($validator_reciever_port+1))
 echo $peer_reciever_flag
 ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$validator_peer_port -o $validator_public_port -t $validator_private_port -r $validator_reciever_port $peer_reciever_flag"
@@ -26,12 +27,15 @@ cd ..
 
 for i in $(seq 1 $(($number_of_validators - 1))); do
     cd backend$i
+    # clear the reciever flags for the new validator
     peer_reciever_flag=""
+    # previously we skipped adding the first validators peer port to the reciever flags. As we are no longer on the first validator, we add the first validators reciever port back into the reciever flag for the next validator
     peer_reciever_flag+="-a 0.0.0.0:"
     peer_reciever_flag+=$validator_reciever_port
     peer_reciever_flag+=" "
     validator_reciever_port=$(($validator_reciever_port+1))
     for i in $(seq 0 $(($number_of_validators-2))); do
+            # do not add reciever port to peer reciever flags if the reciever port (validator_reciever_port) is the same as the current validators reciever port (current_reciever_port)
             if [[ $validator_reciever_port -eq $current_reciever_port ]]
             then
                 validator_reciever_port=$(($validator_reciever_port+1))
