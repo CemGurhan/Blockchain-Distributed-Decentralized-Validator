53183352ff9e6cb7c2b633f7d735059802fbce66
CemGurhan <cemg@live.co.uk>
2022-12-19 15:32:00 +0000

    confirmed working version for 4 validator make file rule

--- backend/test_scripts/run_local_test.sh
+++ backend/test_scripts/run_local_test.sh
@@ -34,7 +34,7 @@ for i in $(seq 1 $(($number_of_validators - 1))); do
     peer_reciever_flag+=$validator_reciever_port
     peer_reciever_flag+=" "
     validator_reciever_port=$(($validator_reciever_port+1))
-    for i in $(seq 0 $(($number_of_validators-2))); do
+    for j in $(seq 0 $(($number_of_validators-2))); do
             # do not add reciever port to peer reciever flags if the reciever port (validator_reciever_port) is the same as the current validators reciever port (current_reciever_port)
             if [[ $validator_reciever_port -eq $current_reciever_port ]]
             then
@@ -47,13 +47,9 @@ for i in $(seq 1 $(($number_of_validators - 1))); do
             validator_reciever_port=$(($validator_reciever_port+1))
     done
     echo $peer_reciever_flag
-    validator_peer_port=$(($validator_peer_port+1))
-    validator_public_port=$(($validator_public_port+2))
-    validator_private_port=$(($validator_private_port+2))
-    # echo $current_reciever_port
-    ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$validator_peer_port -o $validator_public_port -t $validator_private_port -r $current_reciever_port $peer_reciever_flag"
-    # reset validator peer port so that calculation of validator_reciever_port can be reset to the correct value
-    validator_peer_port=6332
+    validator_public_port=$(($validator_public_port+$((i+1))))
+    validator_private_port=$(($validator_private_port+$((i+1))))
+    ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $validator_public_port -t $validator_private_port -r $current_reciever_port $peer_reciever_flag"
     validator_reciever_port=$(($validator_peer_port+$number_of_validators))
     current_reciever_port=$(($current_reciever_port+1))
     cd ..
