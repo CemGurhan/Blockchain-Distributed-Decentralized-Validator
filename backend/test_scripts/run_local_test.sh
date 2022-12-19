e87c8a9c7d97ed3f7dc7adc5985270b2dfd5689c
CemGurhan <cemg@live.co.uk>
2022-12-19 13:10:17 +0000

    altered reciever flag generation in run_local_test.sh to allow validators to call all other validator reciever services on the mock network

--- backend/test_scripts/run_local_test.sh
+++ backend/test_scripts/run_local_test.sh
@@ -5,26 +5,25 @@ number_of_validators=$1
 validator_peer_port=6332
 validator_public_port=9000
 validator_private_port=9001
-validator_reciever_port=6336
+validator_reciever_port=$(($validator_peer_port+$number_of_validators))
 peer_reciever_flag=""
 peer_reciever_port=6337
-current_reciever_port=6336
+current_reciever_port=$(($validator_peer_port+$number_of_validators))
 
-if [[ validator_reciever_port -eq 6336 ]]
-then
-    for i in $(seq 0 $(($number_of_validators - 2))); do
-            peer_reciever_flag+="-a 0.0.0.0:"
-            validator_reciever_port=$(($validator_reciever_port+$((i+1))))
-            peer_reciever_port=$validator_reciever_port
-            peer_reciever_flag+=$peer_reciever_port
-            peer_reciever_flag+=" "
-            validator_reciever_port=$(($validator_reciever_port-$((i+1))))
-    done
-    validator_reciever_port=6336
-    current_reciever_port=$(($validator_reciever_port+1))
-    echo $peer_reciever_flag
-    ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$validator_peer_port -o $validator_public_port -t $validator_private_port -r $validator_reciever_port $peer_reciever_flag"
-fi
+
+for i in $(seq 0 $(($number_of_validators - 2))); do
+        peer_reciever_flag+="-a 0.0.0.0:"
+        validator_reciever_port=$(($validator_reciever_port+$((i+1))))
+        peer_reciever_port=$validator_reciever_port
+        peer_reciever_flag+=$peer_reciever_port
+        peer_reciever_flag+=" "
+        validator_reciever_port=$(($validator_reciever_port-$((i+1))))
+done
+validator_reciever_port=$(($validator_peer_port+$number_of_validators))
+current_reciever_port=$(($validator_reciever_port+1))
+echo $peer_reciever_flag
+ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$validator_peer_port -o $validator_public_port -t $validator_private_port -r $validator_reciever_port $peer_reciever_flag"
+
 
 
 for i in $(seq 1 $(($number_of_validators - 1))); do
@@ -47,56 +46,8 @@ for i in $(seq 1 $(($number_of_validators - 1))); do
             validator_reciever_port=$(($validator_reciever_port+1))
     done
     echo $peer_reciever_flag
-    ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $(($validator_public_port+$i)) -t $(($validator_private_port+$i)) -r $(($validator_reciever_port+$i)) $peer_reciever_flag"
-    validator_reciever_port=6336
+    ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $(($validator_public_port+$((i+1)))) -t $(($validator_private_port+$((i+1)))) -r $current_reciever_port $peer_reciever_flag"
+    validator_reciever_port=$(($validator_peer_port+$number_of_validators))
     current_reciever_port=$(($current_reciever_port+1))
 done
 
-# if [[ validator_reciever_port -eq 6336 ]]
-#     then
-#         for i in $(seq 0 $(($number_of_validators - 2))); do
-#                 peer_reciever_flag+="-a 0.0.0.0:"
-#                 validator_reciever_port=$(($validator_reciever_port+$((i+1))))
-#                 peer_reciever_port=$validator_reciever_port
-#                 peer_reciever_flag+=$peer_reciever_port
-#                 peer_reciever_flag+=" "
-#                 validator_reciever_port=$(($validator_reciever_port-$((i+1))))
-#         done
-#     validator_reciever_port=6336
-#     current_reciever_port=$(($validator_reciever_port+1))
-#     # ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $(($validator_public_port+$i)) -t $(($validator_private_port+$i)) -r $(($validator_reciever_port+$i)) $peer_reciever_flag" 
-# fi
-
-
-# for i in $(seq 0 $(($number_of_validators - 1))); do
-
-#     echo $peer_reciever_flag
-#     # ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $(($validator_public_port+$i)) -t $(($validator_private_port+$i)) -r $(($validator_reciever_port+$i)) $peer_reciever_flag" 
-
-#     while [ $current_reciever_port -lt $((6336 + $number_of_validators)) ]
-#     do
-#         peer_reciever_flag=""
-#         peer_reciever_flag+="-a 0.0.0.0:"
-#         peer_reciever_port=$validator_reciever_port
-#         peer_reciever_flag+=$peer_reciever_port
-#         peer_reciever_flag+=" "
-#         validator_reciever_port=$(($validator_reciever_port+1))
-#         for i in $(seq 0 $(($number_of_validators-2))); do
-#                 if [[ $validator_reciever_port -eq $current_reciever_port ]]
-#                 then
-#                     validator_reciever_port=$(($validator_reciever_port+1))
-#                     continue
-#                 fi
-#                 peer_reciever_flag+="-a 0.0.0.0:"
-#                 peer_reciever_port=$validator_reciever_port
-#                 peer_reciever_flag+=$peer_reciever_port
-#                 peer_reciever_flag+=" "
-#                 validator_reciever_port=$(($validator_reciever_port+1))
-#         done
-#         validator_reciever_port=6336
-#         current_reciever_port=$(($current_reciever_port+1))
-#         echo $peer_reciever_flag
-#         # ttab -w eval "sh test_scripts/configure_node.sh -n $number_of_validators -p 0.0.0.0:$(($validator_peer_port+$i)) -o $(($validator_public_port+$i)) -t $(($validator_private_port+$i)) -r $(($validator_reciever_port+$i)) $peer_reciever_flag"
-#     done
-
-# done
